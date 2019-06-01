c6e23995da578e3ce2b1c6285f8bf42a7afc1ced
areks <arekstanik196@gmail.com>
2019-06-01 17:28:11 +0200

    pierwsza wersja

---
+++ C++ Zelend/Kurs C++ odc 1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "test"<<endl;
+    cout << "------------------------------------------------------------"<<endl;
+    cout << "Ile jest stundetow w twojej uczelni?"<<endl;
+    cin >> a;
+    cout << "Na podstawie twoich dancyh w twoijej uczelni jest tylko:  ";
+    cout << a << "milego dnia";
+
+    cout <<"-------------------------------------------------------------"<<endl;
+
+    int uczniowie,cukierki,x,y;
+    cout <<"Ile uczniow jest w Twojej klasie: ";
+    cin >> uczniowie;
+
+    cout <<"Ile cukierkow kupila Mama?";
+    cin >> cukierki;
+
+    x = cukierki/(uczniowie-1);
+    cout <<"Cukierkow dla kazdego ucznia: "<< x;
+
+    y = cukierki-x*(uczniowie-1);
+    cout <<endl<<"Dla jasie na wieczor zostalo: "<<y ;
+    return 0;
+}
+
+
+
