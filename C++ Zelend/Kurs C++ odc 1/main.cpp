47d2c3084bae28b33e6b74309ac0225df54a079e
areks <arekstanik196@gmail.com>
2019-06-01 18:35:01 +0200

    Ułożenie logicznego sensu zdania i dolozenie nowej zmiennej w celu optymalizacji dziala

--- C++ Zelend/Kurs C++ odc 1/main.cpp
+++ C++ Zelend/Kurs C++ odc 1/main.cpp
@@ -7,24 +7,25 @@ int main()
     int a;
     cout << "test"<<endl;
     cout << "------------------------------------------------------------"<<endl;
-    cout << "Ile jest stundetow w twojej uczelni?     "<<endl;
+    cout << "Ile jest stundetow/uczniow jest w twojej uczelni/szkole?     "<<endl;
     cin >> a;
-    cout << "Na podstawie twoich dancyh w twoijej uczelni jest tylko:      ";
-    cout << a << "milego dnia"<< endl;
+    cout << "Na podstawie twoich dancyh w twoijej uczelni lub szkole jest tylko:      ";
+    cout << a <<endl << "milego dnia"<< endl;
 
     cout <<"-------------------------------------------------------------"<<endl;
 
-    int uczniowie,cukierki,x,y;
-    cout <<"Ile uczniow jest w Twojej klasie:   ";
+    int uczniowie,cukierki,dodatek,x,y;
+    cout <<"Jas ma urodziny i mama powiedziala Jasiowi ze 'bedzie mogl zjesc reszte cukierkow wieczorem po tym jak rozda reszte w szkole na zajeciach wszystkim w klasie i nauczycielce' "<<endl;
+    cout <<"Jak sadzisz ile jest uczniow w klasie Jasia?  ";
     cin >> uczniowie;
 
-    cout <<"Ile cukierkow kupila Mama?     ";
-    cin >> cukierki;
-
-    x = cukierki/(uczniowie-1);
+    cout <<"Jezeli w klasie Jasia jest "<< uczniowie<<" uczniow to mama musi kupic przynajmniej "<< uczniowie <<"   cukierkow, wiec najlepiej bedzie jak dodamy do tej liczby dodatkowe cukierki. Ile mamy ich dolozyc? ";
+    cin >> dodatek;
+    cukierki = uczniowie + dodatek;
+    x = cukierki /(uczniowie-1);
     cout <<"Cukierkow dla kazdego ucznia: "<< x;
 
-    y = cukierki-x*(uczniowie-1);
+    y = cukierki - x *(uczniowie-1);
     cout <<endl<<"Dla jasie na wieczor zostalo: "<<y<<endl;
     cout <<"Dziekuje za uwage";
 
