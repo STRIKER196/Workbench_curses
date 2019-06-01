0af95021a364293bc78ab5d114d6e8c55c81d7f3
areks <arekstanik196@gmail.com>
2019-06-01 17:59:06 +0200

    justowanie tekstu

--- C++ Zelend/Kurs C++ odc 1/main.cpp
+++ C++ Zelend/Kurs C++ odc 1/main.cpp
@@ -7,18 +7,18 @@ int main()
     int a;
     cout << "test"<<endl;
     cout << "------------------------------------------------------------"<<endl;
-    cout << "Ile jest stundetow w twojej uczelni?"<<endl;
+    cout << "Ile jest stundetow w twojej uczelni?     "<<endl;
     cin >> a;
-    cout << "Na podstawie twoich dancyh w twoijej uczelni jest tylko:  ";
+    cout << "Na podstawie twoich dancyh w twoijej uczelni jest tylko:      ";
     cout << a << "milego dnia"<< endl;
 
     cout <<"-------------------------------------------------------------"<<endl;
 
     int uczniowie,cukierki,x,y;
-    cout <<"Ile uczniow jest w Twojej klasie: ";
+    cout <<"Ile uczniow jest w Twojej klasie:   ";
     cin >> uczniowie;
 
-    cout <<"Ile cukierkow kupila Mama?";
+    cout <<"Ile cukierkow kupila Mama?     ";
     cin >> cukierki;
 
     x = cukierki/(uczniowie-1);
