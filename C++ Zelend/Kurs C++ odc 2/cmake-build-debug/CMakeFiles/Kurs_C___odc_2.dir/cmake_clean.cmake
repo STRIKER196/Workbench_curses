b08cab1f994a6bb2ca6b63d1cc2dc0e64b156bd9
areks <arekstanik196@gmail.com>
2019-06-02 10:09:23 +0200

    first package

---
+++ C++ Zelend/Kurs C++ odc 2/cmake-build-debug/CMakeFiles/Kurs_C___odc_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kurs_C___odc_2.dir/main.cpp.obj"
+  "Kurs_C___odc_2.pdb"
+  "Kurs_C___odc_2.exe"
+  "Kurs_C___odc_2.exe.manifest"
+  "libKurs_C___odc_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kurs_C___odc_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++ Zelend/Kurs C++ odc 3/cmake-build-debug/CMakeFiles/Kurs_C___odc_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kurs_C___odc_3.dir/main.cpp.obj"
+  "Kurs_C___odc_3.pdb"
+  "Kurs_C___odc_3.exe"
+  "Kurs_C___odc_3.exe.manifest"
+  "libKurs_C___odc_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kurs_C___odc_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
